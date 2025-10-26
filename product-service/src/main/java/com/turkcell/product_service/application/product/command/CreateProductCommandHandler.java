28017e8f93a31149a8398e645d287e4e7d5f46c9
Eren Sarialp <erensrlp@gmail.com>
2025-10-26 10:59:09 +0300

    Enhance Docker and application configurations: rename database services in docker-compose, add order service database configuration, and update product service settings. Implement message building for Kafka event in OrdersController and add JPA dependencies for both services.

--- product-service/src/main/java/com/turkcell/product_service/application/product/command/CreateProductCommandHandler.java
+++ product-service/src/main/java/com/turkcell/product_service/application/product/command/CreateProductCommandHandler.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 import com.turkcell.product_service.core.cqrs.CommandHandler;
 
 @Component
-public class CreateProductCommandHandler implements CommandHandler {
+public class CreateProductCommandHandler {
 
 }
